db83ba56f950bd7f8b19c973d9bebeda05df8797
arthurarthure <arthurmukiibi08@gmail.com>
2025-11-11 16:17:52 +0100

    Uppg6 done

--- simple_calculator.js
+++ simple_calculator.js
@@ -4,3 +4,21 @@ function calculateSum() {
     let sum = num1 + num2;
     document.getElementById('result').innerHTML = 'Result: ' + sum;
 }
+function calculateDifference() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let sum = num1 - num2;
+    document.getElementById('result').innerHTML = 'Result: ' + sum;
+}
+function calculateFactor() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let sum = num1 * num2;
+    document.getElementById('result').innerHTML = 'Result: ' + sum;
+}
+function calculateDiv() {
+    let num1 = parseFloat(document.getElementById('num1').value);
+    let num2 = parseFloat(document.getElementById('num2').value);
+    let sum = num1 / num2;
+    document.getElementById('result').innerHTML = 'Result: ' + sum;
+}
